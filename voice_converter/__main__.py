de562c9c5d661ed4bad80c5b18e12df12d9c8bcf
oleksandr <prudnikov21@icloud.com>
2023-04-02 19:41:58 +0300

    while true to sound converter added

--- voice_converter/__main__.py
+++ voice_converter/__main__.py
@@ -1,17 +1,20 @@
 import os
 import pathlib
 import subprocess
+import time
 
 voice_messages_path = os.path.join(pathlib.Path(__file__).parent.parent.resolve(), 'voice_files')
 
+while True:
 
-for voice_file in os.listdir(voice_messages_path):
-    if not voice_file.endswith('.oga'):
-        continue
-    input_file = os.path.join(voice_messages_path, voice_file)
+    for voice_file in os.listdir(voice_messages_path):
+        if not voice_file.endswith('.oga'):
+            continue
+        input_file = os.path.join(voice_messages_path, voice_file)
 
-    output_file = os.path.join(voice_messages_path, voice_file.split('.')[0] + '.mp3')
+        output_file = os.path.join(voice_messages_path, voice_file.split('.')[0] + '.mp3')
 
-    # subprocess.call(['/Users/oleksandr/Downloads/ffmpeg', '-i', input_file, '-acodec', 'libmp3lame', '-ab', '192k', output_file])
-    subprocess.call(['ffmpeg', '-i', input_file, '-acodec', 'libmp3lame', '-ab', '192k', output_file])
-    os.remove(input_file)
+        # subprocess.call(['/Users/oleksandr/Downloads/ffmpeg', '-i', input_file, '-acodec', 'libmp3lame', '-ab', '192k', output_file])
+        subprocess.call(['ffmpeg', '-i', input_file, '-acodec', 'libmp3lame', '-ab', '192k', output_file])
+        os.remove(input_file)
+    time.sleep(1)
