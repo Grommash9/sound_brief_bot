7ba6fc78c469f36d62a6e0e63a55fc28f7e1c792
oleksandr <prudnikov21@icloud.com>
2023-04-02 19:26:51 +0300

    new text messages

--- bot_app/handlers/user/base.py
+++ bot_app/handlers/user/base.py
@@ -14,13 +14,17 @@ async def process_start(message: Message, state: FSMContext):
     user_data = await users.create_user(message.from_user)
     await state.finish()
     await bot.send_message(message.from_user.id,
-                           'basic hello message')
+                           'Привіт! Я Telegram-бот, який допомагає з ефективним використанням часу. \n\n'
+                           'Я розшифровую голосові повідомлення та виділяю з них корисну інформацію, '
+                           'щоб ви могли швидко і зручно ознайомитися з контентом, не витрачаючи багато'
+                           ' часу на прослуховування повідомлень. Якщо у вас є голосове повідомлення, '
+                           'яке ви бажаєте розшифрувати, просто перешліть його мені, і я зроблю все решту!')
 
 
-@dp.message_handler(content_types=[types.ContentType.AUDIO, types.ContentType.VOICE])
+@dp.message_handler(content_types=[types.ContentType.VOICE])
 async def handle_voice_message(message: Message):
     await message.voice.download(destination=f"voice_files/{message.chat.id}_{message.message_id}.oga")
     await db_methods.voice_files.create(message)
-    await bot.send_message(message.chat.id, 'Голосовое сообщение получено и будет обработано')
+    await bot.send_message(message.chat.id, 'Голосове повідомлення отримано та буде оброблено, чекайте')
 
 
